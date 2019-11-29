3d28dcdc8f9998eac27c3242fdb16835b267c86f
Julio Valencia <jucevalenciaza@ittepic.edu.mx>
2019-11-28 17:49:47 -0700

    render html

--- index.js
+++ index.js
@@ -1,40 +1,29 @@
-// index.js
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
 
-require('./app/index')
-const _ = require('lodash')
 
-_.assign({ 'a': 1 }, { 'b': 2 }, { 'c': 3 });
+const app = express()
+const port = 3000
 
-const fs = require('fs')
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
 
-console.log('start reading a file...')
-
-fs.readFile('file.md', 'utf-8', function (err, content) {
-  if (err) {
-    console.log('error happened during reading the file')
-    return console.log(err)
-  }
-
-  console.log(content)
+app.get('/', (request, response) => {
+  response.render('home', {
+    name: 'John'
+  })
 })
 
-console.log('end of the file')
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
 
-function stats (file) {
-  return new Promise((resolve, reject) => {
-    fs.stat(file, (err, data) => {
-      if (err) {
-        return reject (err)
-      }
-      resolve(data)
-    })
-  })
-}
-
-Promise.all([
-  stats('file1'),
-  stats('file2'),
-  stats('file3')
-])
-.then((data) => console.log(data))
-.catch((err) => console.log(err))
+  console.log(`server is listening on ${port}`)
+})
